d125e0a4c8e1285cd3add15ddfdea602035ba322
madelinedq <77447725+madelinedq@users.noreply.github.com>
2021-02-03 10:33:38 -0500

    Add files via upload

---
+++ HW2_deQuillacq_Madeline.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+colorList = ["blue", "purple", "pink", "yellow", "green"]
+for color in colorList:
+    print(color)
+
+
+# In[2]:
+
+
+n = 0
+numbers = list(range(10))
+for num in numbers: 
+    print (num)
+
+
+# In[1]:
+
+
+n = 0
+while n<10:
+    print(n)
+    n = n+1
+
+
+# In[1]:
+
+
+n = 0
+while n <= 10:
+    print(n)
+    n = n + 1
+else:
+    print("greater than 10")
+
+
+# In[3]:
+
+
+n = 30
+sum = 0
+i = 1
+
+while i <= n:
+    sum = sum+i
+    i=i+1
+    
+print(sum)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
